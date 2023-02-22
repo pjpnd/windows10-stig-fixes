6112a61db74e1dd4cacb456141013e1397f100f7
pjpnd <105672997+pjpnd@users.noreply.github.com>
2023-02-22 11:55:35 -0500

    Add files via upload

---
+++ win10-stig.ps1
@@ -0,0 +1,11 @@
+﻿#import csv stig registry data, create and set corresponding values
+Import-Csv -Path .\win10-stig-registry.csv | ForEach {
+   If (!(Test-Path $_.RegPath)) {
+    New-Item -path $_.RegPath -force | out-null
+    }
+
+    New-ItemProperty -path $_.RegPath -name $_.ValueName -value $_.Value -PropertyType $_.PropType -force
+} 
+
+#stop and disable the seclogon service 
+Set-Service -name seclogon -Status Stopped -StartupType Disabled
